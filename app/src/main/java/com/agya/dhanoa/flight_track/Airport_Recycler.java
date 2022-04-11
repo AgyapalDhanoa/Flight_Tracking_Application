82a80a7701ad5859d3f562ff4ac1dc1b1f973267
agyapal <agyapaldhanoa@gmail.com>
2022-04-11 17:17:55 -0600

    Recyclers

---
+++ app/src/main/java/com/agya/dhanoa/flight_track/Airport_Recycler.java
@@ -0,0 +1,14 @@
+package com.agya.dhanoa.flight_track;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Airport_Recycler extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_airport_recycler);
+    }
+}
