b38122dd2b5f652cf8b9787d443b8f4d1a86a1ee
agyapal <agyapaldhanoa@gmail.com>
2022-04-15 01:38:47 -0600

    Recyclers

--- app/src/main/java/com/agya/dhanoa/flight_track/AirlineAdapter.java
+++ app/src/main/java/com/agya/dhanoa/flight_track/AirlineAdapter.java
@@ -1,28 +1,24 @@
 package com.agya.dhanoa.flight_track;
 
 
+import android.annotation.SuppressLint;
 import android.content.Context;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.squareup.picasso.Picasso;
-
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 
-
 public class AirlineAdapter extends RecyclerView.Adapter<AirlineAdapter.AirlineViewHolder> {
-    private Context mContext;
-    private ArrayList<AirlineItem> mExampleList;
-private OnAirlineListener monAirlineListener;
+    private final Context mContext;
+    private final ArrayList<AirlineItem> mExampleList;
+private final OnAirlineListener monAirlineListener;
 
 public AirlineAdapter(Context context, ArrayList<AirlineItem> exampleList,OnAirlineListener onAirlineListener) {
         mContext = context;
@@ -30,19 +26,21 @@ public AirlineAdapter(Context context, ArrayList<AirlineItem> exampleList,OnAirl
         this.monAirlineListener = onAirlineListener;
     }
 
+    @NonNull
     @Override
-    public AirlineViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public AirlineViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(mContext).inflate(R.layout.airport_item, parent, false);
-        return new AirlineViewHolder(v,monAirlineListener);
+        return new AirlineViewHolder(v, monAirlineListener);
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(AirlineViewHolder holder, int position) {
         AirlineItem currentItem = mExampleList.get(position);
 
-        String Airport = currentItem.getmAirport();
-        String Code = currentItem.getmCode();
-        String Title = currentItem.getmTitle();
+        String Airport = currentItem.getAirport();
+        String Code = currentItem.getCode();
+        String Title = currentItem.getTitle();
 
         holder.mAirport.setText("Airport:"+Airport);
         holder.mCode.setText("Code:" + Code);
@@ -62,7 +60,7 @@ public AirlineAdapter(Context context, ArrayList<AirlineItem> exampleList,OnAirl
     }
 
 
-    public class AirlineViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public static class AirlineViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
          TextView mAirport,mCode,mTitle;
          ImageView Images;
         OnAirlineListener onAirlineListener;
